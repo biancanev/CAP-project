1f3be32754b2406d7e549c47fb860e16138e5756
Biancano <bebostunner234@gmail.com>
2018-04-21 18:04:29 -0700

    Update node.js

--- assets/node.js
+++ assets/node.js
@@ -4,8 +4,14 @@ var x = require("port");
 const(x) = function(a,b){
   while(a<b){
     http.createServer(function (req, res) {
-      res.write('Hello World!'); //write a response to the client
+      if(req == 'payment'){
+        res.write('Payement Succesful!'); //write a response to the client
+        res.end;
+      }else{
+      window.location.href('https://biancanev.github.io/pay');
+      res.write(err-msg);
       res.end(); //end the response
+      }
     }).listen(8080); //the server object listens on port 8080
   }
 };
