fd031e417a84701ad381ae6942bf1797a123d63a
Biancano <bebostunner234@gmail.com>
2018-04-21 17:47:00 -0700

    Create node.js

---
+++ assets/node.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var x = require("port");
+const(x) = function(a,b){
+  while(a<b){
+    http.createServer(function (req, res) {
+      res.write('Hello World!'); //write a response to the client
+      res.end(); //end the response
+    }).listen(8080); //the server object listens on port 8080
+  }
+};
