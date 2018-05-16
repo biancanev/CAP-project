0a5c96536c2cf4cd9c72079f62a75aa0643022bb
Biancano <bebostunner234@gmail.com>
2018-05-15 18:07:26 -0700

    Create paypal_config.js

---
+++ assets/paypal_config.js
@@ -0,0 +1,20 @@
+const id = "A4Dglkaj3Dae8nlmChwKrj235";
+const http = require('http');
+function query(e,i){
+  while(i > id.value){
+    e.create('div')
+    e.div.className = "processor";
+    if(id === http.id){
+      console.log(id.msg);
+      window.location.href="/paypal_success_msg";
+      document.clearCache(id);
+      if(id.cache = ""){
+        console.log(id.msg);
+      }
+    }else{
+      console.log(err.msg);
+      document.getElementByTagName("input").value = "";
+      alert(err.msg);
+    }
+  }
+}
