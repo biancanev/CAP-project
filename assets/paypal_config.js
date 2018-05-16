e9ec912522675378e64852384528c7ecaeaae22b
Biancano <bebostunner234@gmail.com>
2018-05-16 15:45:49 -0700

    Update paypal_config.js

--- assets/paypal_config.js
+++ assets/paypal_config.js
@@ -1,14 +1,31 @@
+//Install paypal package
 var package = url("/a/package_json/api4_/paypal/valueFrame.json");
 package.type = "json";
+//Set input error trials
+var errNum = 0;
+//set encryption keys
+var int = 0;
+var numArray = [0,1,2,3,4,5,6,7,8,9];
+var charArray = ["a","b","c","d","e"
+                 ,"f","g","h","i","j"
+                 ,"k","l","m","n","o"
+                 ,"p","q","r","s","t"
+                 ,"u","v","w","x","y"
+                 ,"z"];
+//Paypal verification constants
 const id = "A4Dglkaj3Dae8nlmChwKrj235";
 const http = require('http');
-function loading(value,ping){
+//Secure a connection to paypal pay services
+function loading(value,ping,con){
   var i = document.bufferFrameRate(1600);
   if(ping + value < i){
     parseInt(ping);
     parseInt(value);
     if(ping < value){
       alert("Your ping rate is too low! Your ping rate: " + ping);
+      if(con < 1400){
+        alert("Your connection is to slow. Your internet speed is: " + con + "mbps");
+      }
     }if(value < ping){
       alert("Your computer is too slow! Your computer speed is: " + value);
     }else{
@@ -16,8 +33,9 @@ function loading(value,ping){
     }
   }
 }
+//Send encrypted data to paypal
 function query(e,i){
-  while(i > id.value){
+  while(i > id.value && package === "json"){
     e.create('div')
     e.div.className = "processor";
     if(id === http.id){
@@ -31,8 +49,18 @@ function query(e,i){
           if(i.class === "resolved" && e.class === "resolved"){
             resolve.http.send(id.msg);
             while(response.http != "success"){
-             loading(e); 
+             loading(e,i,window.connection); 
              console.resolveData(i,3,id);
+            }
+            while(int < id.value.legnth){
+              document.setCookie(cryption);
+              if(id.value[int] != numArray){
+               parseInt(int);
+               e = e + i + int;
+               id.value[int]=id.value[int+e]; 
+              }else{
+               id.value[int]=id.value[int+e]; 
+              }
             }
             alert("Success!");
           }
